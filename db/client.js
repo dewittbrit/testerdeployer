caeddde1a70b2901b5d8a2cb91128a47444ad86b
dewittbrit <szabobrit@gmail.com>
2024-03-21 19:09:32 -0400

    COMMIT

--- db/client.js
+++ db/client.js
@@ -3,7 +3,8 @@ require("dotenv").config()
 
 let DATABASE_URL = process.env.DATABASE_URL || "postgres://localhost:5432/luigis_pizzeria"
 
-// console.log(DATABASE_URL)
+console.log(DATABASE_URL)
+
 const client = new Client({ connectionString: DATABASE_URL, ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : undefined, })
 
 // const client = new Client("postgres://localhost:5432/luigis_pizzeria" )
